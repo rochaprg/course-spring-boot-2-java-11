2b82cd9cdedabd63cc754b943443987de2ef6c0d
Paulo Rocha <rochaprg@gmail.com>
2021-08-24 16:15:56 -0300

    Service layer, component registration

---
+++ src/main/java/com/paulorocha/course/services/UserService.java
@@ -0,0 +1,31 @@
+package com.paulorocha.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulorocha.course.entity.User;
+import com.paulorocha.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		
+		return repository.findAll();
+		
+	}
+	
+	public User findById (Long id) {
+		
+		Optional<User> obj = repository.findById(id);
+		
+		return obj.get();
+	}
+
+}
